5a43ae02944e2a14a517ab434efa2e140372006f
KARLA MARÍA <kmaria.grosales@gmail.com>
2025-01-29 18:08:48 -0600

    PRÁCTICA2

--- FAST/main.py
+++ FAST/main.py
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {'hello': 'world fastApi'}
+
+#EndPoint promedio
+@app.get('/promedio')
+def promedio():
+    return 10.5
+
+#EndPoint con parámetro obligatorio
+@app.get('/usuario/{id}')
+def consultausuario(id:int):
+    #caso ficticio de búsqueda en BD
+    return {"Se encontró el usuario":id}
